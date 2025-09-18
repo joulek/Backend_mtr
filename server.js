8bfbc1ac6d6ad84caff2a642a16b05dea270975a
nourhene <nourheneabbes12@gmail.com>
2025-09-18 13:20:39 +0100

    j

--- server.js
+++ server.js
@@ -87,11 +87,12 @@ app.use("/api/devis/compression", devisCompressionRoutes);
 app.use("/api/devis/grille", devisGrilleRoutes);
 app.use("/api/devis/filDresse", devisFillDresseRoutes);
 app.use("/api/devis/autre", devisAutreRoutes);
+app.use("/api", mesDemandesDevisRoutes);
 app.use("/api/devis", devisRoutes);
 app.use("/api", signedFilesRoutes);
 app.use("/api/reclamations", auth, upload.array("piecesJointes"), reclamationRoutes);
 app.use("/api/admin/users", userRoutes);
-app.use("/api", mesDemandesDevisRoutes);
+
 app.use("/api/order", clientOrderRoutes);
 app.use("/api/contact", contactRoutes);
 app.use("/api/dashboard", dashboardRoutes);
